7e30abc7537e9b6274768f3cf6ed1e1053390ad9
brsferreira <bsf.6626@gmail.com>
2020-11-23 22:05:55 -0300

    Using try with resources

--- src/application/CopyFile.java
+++ src/application/CopyFile.java
@@ -17,14 +17,12 @@ public final class CopyFile {
 	public static void main(String[] args) {
 		
 		int i;
-		FileInputStream fin = null;
-		FileOutputStream fout = null;
 		
-		try {
+		try(FileInputStream fin = new FileInputStream("..\\CopyFile\\src\\application\\frist.txt");
+				FileOutputStream fout = new FileOutputStream("..\\CopyFile\\src\\application\\second.txt")) {
 			//data input
-			fin = new FileInputStream("..\\CopyFile\\src\\application\\frist.txt");
 			//data output
-			fout = new FileOutputStream("..\\CopyFile\\src\\application\\second.txt");
+			
 			
 			do {
 				i = fin.read();
@@ -34,17 +32,6 @@ public final class CopyFile {
 			
 		} catch (IOException e) {
 			System.out.println("ERROR I/O: " + e.getMessage() + e.getCause());
-		}finally {
-			try {
-				if(fin != null) fin.close();;
-			} catch (IOException e) {
-				System.out.println("ERROR I/O: in input file" + e.getMessage());
-			}
-			try {
-				if(fout != null) fout.close();
-			} catch (Exception e2) {
-				System.out.println("ERROR I/O: in output file" + e2.getMessage());
-			}
 		}
 	}//main
 }//class
